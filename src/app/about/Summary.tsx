8a443d01c64fcbfeec203dafb0d899b7c985fce5
a-gaoba <abdulrahman1997abdo@gmail.com>
2025-01-19 01:30:11 +0300

    fixed

--- src/app/about/Summary.tsx
+++ src/app/about/Summary.tsx
@@ -5,7 +5,7 @@ import TrueFocus from './TrueFocus';
 function Summary() {
   return (
     <div className="max-w-4xl mx-auto ">
-      <h2 className="text-2xl font-bold dark:text-white md:mb-6">About Me</h2>
+      <h2 className="text-2xl font-bold dark:text-white md:mb-6 mb-2">About Me</h2>
       <TrueFocus
         sentence="Welcome to my world of innovation and creativity! As a passionate software engineer and web developer with a keen eye for captivating UI/UX design, I bring dreams to life through elegant and functional solutions. With a relentless drive to deliver excellence, I thrive on crafting seamless user experiences that leave a lasting impression. Explore my portfolio, and let’s embark on an extraordinary digital journey together."
       />
